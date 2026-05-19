91499611ba7d8a553435bffe22b3a71eaf77d5c5
yashdeore69 <deoreyash2003@gmail.com>
2026-05-19 18:54:12 +0530

    connected db

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,19 +1,35 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.StudentDto;
+import com.example.demo.repository.StudentRepository;
+import com.example.demo.entity.Student;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 
 @RestController
 public class StudentController {
 
+    private final StudentRepository studentRepository;
+
+    public StudentController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     @GetMapping("/student")
-    public StudentDto getStudent() {
-        return new StudentDto(1234L, "Yash", "deoreyash@gmail.com");
+    public List<Student> getStudent() {
+        return studentRepository.findAll();
 
     }
+
+    // @GetMapping("/student/{id}")
+    // public StudentDto getStudentById() {
+    //     return new StudentDto(1234L, "Yash", "deoreyash@gmail.com");
+
+    // }
     
 
 }
